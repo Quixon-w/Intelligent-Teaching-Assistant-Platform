e3b8fa0cca2c2e770f72818fd0b17e685722000b
RSCML <454020090@qq.com>
2025-06-20 16:26:50 +0800

    2025.6.20下午
    完成了登录注册界面样式，部分完成dashboard样式

--- vue-ui/src/components/login/Login.vue
+++ vue-ui/src/components/login/Login.vue
@@ -2,8 +2,8 @@
 import {useRouter} from 'vue-router'
 import { reactive } from 'vue'
 const router = useRouter();
-const form = reactive({
-  name: '',
+const loginForm = reactive({
+  username: '',
   passsword: '',
 })
 const onLogin = () => {
@@ -15,18 +15,43 @@ const onRegister = () => {
 }
 </script>
 <template>
-  <el-form :model="form" label-width="auto" style="max-width: 600px">
-    <el-form-item label="账户：">
-      <el-input v-model="form.name" />
-    </el-form-item>
-    <el-form-item label="密码：">
-      <el-input v-model="form.passsword" />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="onLogin">登录</el-button>
-      <el-button type="info" @click="onRegister">注册</el-button>
-    </el-form-item>
-  </el-form>
+  <div class="login">
+    <el-form :model="loginForm" class="login-form">
+      <h3 class="title">登录界面</h3>
+      <el-form-item prop="username" label="账户：">
+        <el-input v-model="loginForm.username"/>
+      </el-form-item>
+      <el-form-item prop="password" label="密码：">
+        <el-input v-model="loginForm.passsword" />
+      </el-form-item>
+      <el-form-item prop="onClick">
+        <el-button type="primary" @click="onLogin" style="width: 100%">登录</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="info" @click="onRegister" style="width: 100%">注册</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <style scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-image: url("src/assets/images/login-background.jpg");
+  background-size: cover;
+}
+.title {
+  margin: 0px auto 30px auto;
+  text-align: center;
+  color: #707070;
+}
+
+.login-form {
+  border-radius: 6px;
+  background: #ffffff;
+  width: 400px;
+  padding: 25px 25px 5px 25px;
+}
 </style>
