93119c1f405ebc4009dd7433d4d10d3ecedc3720
RSCML <454020090@qq.com>
2025-06-23 16:31:28 +0800

    save

--- vue-ui/src/utils/request.js
+++ vue-ui/src/utils/request.js
@@ -3,9 +3,5 @@ const service = axios.create({
   baseURL: 'http://192.168.240.226:8080/',
   timeout: 10000,
   withCredentials: true,
-  crossDomain: true,
-  headers: {
-    'Content-Type': 'application/json;charset=utf-8'
-  },
 })
 export default service;
